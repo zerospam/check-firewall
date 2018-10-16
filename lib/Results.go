34bd578769088d3b762f37911c7499a2236a7640
Antoine Aflalo <antoine.aflalo@zerospam.ca>
2018-10-16 12:06:46 -0400

    feat(CheckConnection): Check if we can connect to the server
    It gather all the IPs and do a connection check on all of them.
    A success is considered if we can't connect to the ips.

---
+++ lib/Results.go
@@ -0,0 +1,19 @@
+package lib
+
+import "net"
+
+type NameIp struct {
+	Name string `json:"server"`
+	IP   net.IP `json:"ip"`
+}
+
+type ServerResult struct {
+	*NameIp
+	Success bool `json:"result"`
+}
+type CheckResult struct {
+	Request *TransportServer `json:"request"`
+	Success bool             `json:"success"`
+	Reason  string           `json:"error_message,omitempty"`
+	Results []ServerResult   `json:"details,omitempty"`
+}
--- lib/TransportServer.go
+++ lib/TransportServer.go
@@ -1,6 +1,85 @@
 package lib
 
+import (
+	"errors"
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+)
+
 type TransportServer struct {
-	Transport string `json:"transport"`
-	Port      int    `json:"transport_port"`
+	Server string `json:"server"`
+	Port   int    `json:"port"`
+	OnMx   bool   `json:"mx"`
+}
+
+func (t *TransportServer) Address(server string) string {
+	builder := strings.Builder{}
+	builder.WriteString(server)
+	builder.WriteString(":")
+	builder.WriteString(strconv.Itoa(t.Port))
+	return builder.String()
+}
+
+func (t *TransportServer) isIp() bool {
+	addr := net.ParseIP(t.Server)
+	return addr != nil
+}
+
+func (t *TransportServer) getNames() (names []NameIp, err error) {
+
+	if t.isIp() {
+		names = []NameIp{
+			NameIp{Name: t.Server, IP: net.ParseIP(t.Server)},
+		}
+	} else {
+
+		if t.OnMx {
+			var mxRecords []*net.MX
+			mxRecords, err = net.LookupMX(t.Server)
+			for _, mx := range mxRecords {
+				var ipRecords []net.IP
+				ipRecords, err = net.LookupIP(strings.TrimRight(mx.Host, "."))
+				for _, ip := range ipRecords {
+					names = append(names, NameIp{Name: mx.Host, IP: ip})
+				}
+			}
+
+		} else {
+			var ipRecords []net.IP
+			ipRecords, err = net.LookupIP(t.Server)
+			for _, ip := range ipRecords {
+				names = append(names, NameIp{Name: t.Server, IP: ip})
+			}
+		}
+
+		if err != nil || len(names) == 0 {
+			return nil, errors.New("can't find servers")
+		}
+	}
+	return names, nil
+}
+
+//Check if we can connect to the servers
+func (t *TransportServer) CheckServer() CheckResult {
+	var results []ServerResult
+	var finalResult = true
+
+	names, err := t.getNames()
+	if err != nil {
+		return CheckResult{Request: t, Success: false, Reason: fmt.Sprint(err)}
+	}
+
+	for _, server := range names {
+		conn, err := net.DialTimeout("tcp", t.Address(server.IP.String()), 2*time.Second)
+		if conn != nil {
+			conn.Close()
+		}
+		finalResult = (err != nil) && finalResult
+		results = append(results, ServerResult{NameIp: &server, Success: err != nil})
+	}
+
+	return CheckResult{Request: t, Success: finalResult, Results: results}
 }
