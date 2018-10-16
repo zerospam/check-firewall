7e7463d968da7b0081c9c58319cff93d5b8843a3
Antoine Aflalo <antoine.aflalo@zerospam.ca>
2018-10-16 12:07:42 -0400

    feat(Response): Returns a response to the request

--- main.go
+++ main.go
@@ -2,58 +2,35 @@ package main
 
 import (
 	"CheckFirewall/lib"
-	b64 "encoding/base64"
-	"fmt"
-	"github.com/vmihailenco/msgpack"
-	"github.com/zerospam/rsa/rsa"
-	"io/ioutil"
+	"encoding/json"
 	"net/http"
+	"os"
 )
 
+func init() {
+	http.HandleFunc("/check", checkTransport)
+}
+
+func checkTransport(w http.ResponseWriter, req *http.Request) {
+	var transportServer lib.TransportServer
+
+	if req.Body == nil {
+		http.Error(w, "Please send a request body", 400)
+		return
+	}
+
+	if req.Header.Get("Authorization") != os.Getenv("SHARED_KEY") {
+		http.Error(w, "Wrong Key sent.", 402)
+		return
+	}
+
+	json.NewDecoder(req.Body).Decode(&transportServer)
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(transportServer.CheckServer())
+
+}
+
 func main() {
-	http.HandleFunc("/check", func(w http.ResponseWriter, req *http.Request) {
-		var transportServer lib.TransportServer
-
-		requestBody, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			panic(err)
-		}
-
-		sDecode, err := b64.StdEncoding.DecodeString(string(requestBody))
-
-		if err != nil {
-			panic(err)
-		}
-
-		decrypted, err := rsa.PublicDecrypt(sDecode, "./public.pem", rsa.RSA_PKCS1_PADDING)
-		if err != nil {
-			panic(err)
-		}
-
-		err = msgpack.Unmarshal(decrypted, &transportServer)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Fprintf(w, "%s %d", transportServer.Transport, transportServer.Port)
-	})
-
-	http.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request) {
-		transport := lib.TransportServer{
-			Port:      25,
-			Transport: "mail.example.com",
-		}
-
-		msg, err := msgpack.Marshal(transport)
-		if err != nil {
-			panic(err)
-		}
-		encrypted, err := rsa.PrivateEncrypt([]byte(msg), "./private.pem", rsa.RSA_PKCS1_PADDING)
-		if err != nil {
-			panic(err)
-		}
-		sEnc := b64.StdEncoding.EncodeToString(encrypted)
-		fmt.Fprint(w, sEnc)
-	})
-
 	http.ListenAndServe(":8082", nil)
 }
